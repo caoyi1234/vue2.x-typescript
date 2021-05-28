081b592a6fb0db6ef4957322e5129cf07a5dbf78
caoyi1234 <claire_y@126.com>
2021-05-28 17:55:51 +0800

    first commit

---
+++ src/components/user.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    id:{{id}}
+    姓名:{{userName}}
+    性别:{{SEX[sex]}}
+    年龄:{{age}}
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue, Prop } from 'vue-property-decorator'
+@Component({
+  name: 'User'
+})
+enum Sex {
+  '男' = 1,
+  '女' = 2
+}
+
+export default class User extends Vue {
+  @Prop(Number)
+  id!: number
+
+  @Prop(String)
+  userName!: string
+
+  @Prop({ type: Number, default: 1 })
+  sex?: number
+
+  @Prop(Number)
+  age!: number
+
+  SEX: any = Sex
+}
+</script>
---
+++ src/views/About.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <User
+      v-for="item in userInfo"
+      :userName="item.name"
+      :age="item.age"
+      :sex="item.sex"
+      :id="item.id"
+    ></User>
+  </div>
+</template>
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+import User from '../components/user.vue'
+interface peopleInfo {
+  name: string
+  age: number
+  sex?: number
+  id: number
+}
+@Component({
+  name: 'aboutItem',
+  components: {
+    User
+  }
+})
+export default class extends Vue {
+  userInfo: Array<peopleInfo> = [
+    {
+      id: 1,
+      name: 'aaa',
+      age: 11,
+      sex: 1
+    },{
+      id: 2,
+      name: 'bbb',
+      age: 22,
+      sex: 1
+    },{
+      id: 3,
+      name: 'ccc',
+      age: 33,
+      sex: 2
+    },
+  ]
+}
+</script>
